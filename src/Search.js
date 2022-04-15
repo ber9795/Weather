f02d13adac00f2ce2c2759044cf4123b79342555
Bernardo Acevedo <31902424+ber9795@users.noreply.github.com>
2022-04-15 09:06:09 -0300

    ident

--- src/Search.js
+++ src/Search.js
@@ -1,58 +1,34 @@
-import {useState} from 'react';
+import { useState } from "react";
 
+function Search(props) {
+  const [cidade, setCidade] = useState("");
 
+  function searchInput(e) {
+    e.preventDefault();
 
-function Search(props){
+    //setCidade("");
 
-    
+    let currentValue = document.querySelector("input[name=searchInput]").value;
 
-    const [cidade,setCidade] = useState("");
-
-
-
-    function searchInput(e){
-
-        e.preventDefault();
-
-        //setCidade("");
-
-        let currentValue = document.querySelector('input[name=searchInput]')
-
-        .value;
-
-        
-
-        /*
+    /*
 
             Fazer requisição API depois.
 
         */
 
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
 
-        fetch(url)
+    fetch(url)
+      .then((response) => response.json())
 
-        .then(response=> response.json())
+      .then((data) => {
+        const { main, name, sys, weather } = data;
 
-        .then(data=>{
+        if (sys != undefined) {
+          if (weather != undefined) {
+            const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
 
-            
-
-            const {main, name, sys, weather} = data;
-
-            if(sys != undefined){
-
-                
-
-            if(weather != undefined){
-
-                
-
-                const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-
-             weather[0]["icon"]}.svg`;
-
-                setCidade(`
+            setCidade(`
 
                 <div class="containerCidade">
 
@@ -62,78 +38,53 @@ function Search(props){
 
                 <p>${name}</p>
 
-                <p>${weather[0]['description']}</p>
+                <p>${weather[0]["description"]}</p>
 
                 <img src="${icon}" />
 
                 </div>
 
                 `);
-
-
-
-            }
-
-
-
-            }else{
-
-                setCidade("");
-
-            }
-
-        })
-
-    
-
-    }
-
-
-
-    return(
-
-        <div className="searchWraper">
-
-        <div className="search">
-
-            <h2>Digite a cidade que você quer saber a previsão...</h2>
-
-            <form onSubmit={(e)=>searchInput(e)}>
-
-            <input placeholder={props.placeholder}  type="text" name="searchInput" />
-
-            <input type="submit" style={{width: '80%',
-                height: '40px',
-             
-              border:0,
-              margin: '10px'}} value="Pesquisar por cidade!" />
-
-            </form>
-
-           
-
-        </div>
-
-
-
-        {
-
-            (cidade!= "")?
-
-            <div dangerouslySetInnerHTML={{__html: cidade}} />:
-
-            <div>Pesquise por algo acima...</div>
-
+          }
+        } else {
+          setCidade("");
         }
+      });
+  }
 
+  return (
+    <div className="searchWraper">
+      <div className="search">
+        <h2>Digite a cidade que você quer saber a previsão...</h2>
+        <form onSubmit={(e) => searchInput(e)}>
+          <input
+            placeholder={props.placeholder}
+            type="text"
+            name="searchInput"
+          />
 
+          <input
+            type="submit"
+            style={{
+              width: "80%",
+              height: "40px",
 
-        </div>
+              border: 0,
+              margin: "10px",
+            }}
+            value="Pesquisar por cidade!"
+          />
+        </form>
+        
+      </div>
 
-    )
-
+      {cidade != "" ? (
+        <div dangerouslySetInnerHTML={{ __html: cidade }} />
+      ) : (
+        <div>Pesquise por algo acima...</div>
+      )}
+    </div>
+  );
 }
-
-
 
 export default Search;
